04abe78c834ea2af34cc2f8b3f2b207d0795194e
Ryan Shea <ryan@shea.io>
2016-10-12 18:41:20 -0400

    add in smooth scrolling and scroll spying

--- app/js/pages/HomePage.js
+++ app/js/pages/HomePage.js
@@ -1,7 +1,7 @@
 'use strict'
 
 import React         from 'react'
-import {Link}        from 'react-router'
+import {Link, Element, Events, scrollSpy}        from 'react-scroll'
 import DocumentTitle from 'react-document-title'
 import Header        from '../components/Header'
 import {drawChart} from '../utils/charts.js'
@@ -20,6 +20,22 @@ class HomePage extends React.Component {
     google.charts.load("visualization", "1.0", {packages:["corechart"]});
     // Set a callback to run when the Google Visualization API is loaded.
     google.charts.setOnLoadCallback(drawChart);
+
+    Events.scrollEvent.register('begin', (to, element) => {
+      console.log("begin", arguments);
+    });
+
+    Events.scrollEvent.register('end', (to, element) => {
+      console.log("end", arguments);
+    });
+
+    scrollSpy.update();
+
+  }
+
+  componentWillUnmount() {
+    Events.scrollEvent.remove('begin');
+    Events.scrollEvent.remove('end');
   }
 
   render() {
@@ -33,45 +49,50 @@ class HomePage extends React.Component {
 
               <div className="col-lg-3 col-md-4 hidden-sm-down">
                 <div className="btn-group-vertical" style={{ position: 'fixed', paddingTop: '70px' }}>
-                  <a href="#chart_1" className="btn btn-secondary anchor">
+                  <Link to="chart_1" smooth={true} spy={true}
+                    className="btn btn-secondary anchor">
                     Transaction Growth
-                  </a>
-                  <a href="#chart_2" className="btn btn-secondary anchor">
+                  </Link>
+                  <Link to="chart_2" smooth={true} spy={true}
+                    className="btn btn-secondary anchor">
                     Transaction Growth (log)
-                  </a>
-                  <a href="#chart_3" className="btn btn-secondary anchor">
+                  </Link>
+                  <Link to="chart_3" smooth={true} spy={true}
+                    className="btn btn-secondary anchor">
                     Growth by Protocol
-                  </a>
-                  <a href="#chart_4" className="btn btn-secondary anchor">
+                  </Link>
+                  <Link to="chart_4" smooth={true} spy={true}
+                    className="btn btn-secondary anchor">
                     Breakdown by Protocol
-                  </a>
-                  <a href="#chart_5" className="btn btn-secondary anchor">
+                  </Link>
+                  <Link to="chart_5" smooth={true} spy={true}
+                    className="btn btn-secondary anchor">
                     Last Week's Breakdown
-                  </a>
+                  </Link>
                 </div>
               </div>
               <div className="col-lg-9 col-md-8">
                 <div>
-                  <div id="chart_1">
+                  <Element name="chart_1" className="element">
                     <h3 className="chart-header">Transaction Growth</h3>
                     <div id="total_div" className="chart"></div>
-                  </div>
-                  <div id="chart_2">
+                  </Element>
+                  <Element name="chart_2" className="element">
                     <h3 className="chart-header">Transaction Growth (log)</h3>
                     <div id="total_div_log" className="chart"></div>
-                  </div>
-                  <div id="chart_3">
+                  </Element>
+                  <Element name="chart_3" className="element">
                     <h3 className="chart-header">Growth by Protocol</h3>
                     <div id="proto_div" className="chart"></div>
-                  </div>
-                  <div id="chart_4">
+                  </Element>
+                  <Element name="chart_4" className="element">
                     <h3 className="chart-header">Breakdown by Protocol</h3>
                     <div id="cumulative_div" className="chart"></div>
-                  </div>
-                  <div id="chart_5">
+                  </Element>
+                  <Element name="chart_5" className="element">
                     <h3 className="chart-header">Last Week's Breakdown</h3>
                     <div id="week_div" className="chart"></div>
-                  </div>
+                  </Element>
                 </div>
               </div>
 
